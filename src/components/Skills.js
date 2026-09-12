cfc13f8db4d4dd15213ed47d3732cc83364001f7
rabohlalemojakisane-arch <rabohlalemojakisane@gmail.com>
2026-09-12 08:42:27 +0200

    Add Skills component to display a list of skills

---
+++ src/components/Skills.js
@@ -0,0 +1,13 @@
+function Skills({ skills }) {
+  return (
+    <section>
+      <h2>Skills</h2>
+      <ul className="skills-list">
+        {skills.map((skill, index) => (
+          <li key={index}>{skill}</li>
+        ))}
+      </ul>
+    </section>
+  );
+}
+export default Skills;
